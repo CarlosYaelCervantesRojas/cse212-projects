85d0060400029d06edf8bee88359bfdb1ec8560e
CarlosYaelCervantesRojas <carlos.yael.cervantes.rojas@gmail.com>
2025-03-04 12:42:02 -0600

    W01 Code: Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,24 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // 1.- Create an array of size 'length'
+        double[] multiples = new double[length];
+
+        // 2.- Initialize the index of the 'multiples' array to 0
+        int multipleIndex = 0;
+        // 3.- Loop 'length' times (the number of multiples we need) :
+            // Initialize the loop at 1, because we want the first multiple of any number
+            // Finish the loop at 'length + 1' since we started the loop at 1
+        for (int i = 1; i < length + 1; i++)
+        {
+            // Add the multiple (number * i) to the 'multiples' array 
+            multiples[multipleIndex] = number * i;
+            // Increment the 'multipleIndex' variable by 1
+            multipleIndex++;
+        }
+
+        // Return the 'multiples' array
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +46,17 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // 1.- Create a temporary list to store the elements that will be rotated
+        List<int> rotateData = new List<int>();
+
+        //  2.- Extract the last 'amount' elements from the original list
+        rotateData = data.GetRange(data.Count - amount, amount);
+
+        //  3.- Remove the extracted elements from the original list
+        data.RemoveRange(data.Count - amount, amount);
+        
+        //  4.- Insert the rotated elements at the beginning of the original list
+        data.InsertRange(0, rotateData);
     }
 }
