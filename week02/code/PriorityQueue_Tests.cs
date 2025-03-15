6f393af3339b36161e0266bb6e6d6d9952a68ce7
CarlosYaelCervantesRojas <carlos.yael.cervantes.rojas@gmail.com>
2025-03-15 13:09:44 -0600

    W02 Code: Queues

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,65 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
+    // Scenario: add items with ascendent priority
+    // Expected Result: remove items in descendent priority
     // Defect(s) Found: 
+    //  1.- the dequeue method does not loop until the last item in the queue
+    //  2.- the dequeue method does not removes the item with higher priority before returning the value
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("value1", 1);
+        priorityQueue.Enqueue("value2", 2);
+        priorityQueue.Enqueue("value3", 3);
+
+        string[] expectedResult = ["value3", "value2", "value1"];
+        
+        for (int i = 0; i < priorityQueue.Length; i++)
+        {
+            var value = priorityQueue.Dequeue();
+            Assert.AreEqual(expectedResult[i], value);   
+        }
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: there are more than one item with the highest priority
+    // Expected Result: the item closest to the front of the queue will be removed and its value returned
+    // Defect(s) Found: the item closest to the back is removed 
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("value2", 2);
+        priorityQueue.Enqueue("FirstOut", 3);
+        priorityQueue.Enqueue("value1", 1);
+        priorityQueue.Enqueue("SecondOut", 3);
+
+        string[] expectedResult = ["FirstOut", "SecondOut", "value2", "value1"];
+        
+        for (int i = 0; i < priorityQueue.Length; i++)
+        {
+            var value = priorityQueue.Dequeue();
+            Assert.AreEqual(expectedResult[i], value);   
+        }
     }
 
     // Add more test cases as needed below.
+
+    [TestMethod]
+    // Scenario: the queue is empty
+    // Expected Result: an error exception shall be thrown
+    // Defect(s) Found: none
+    public void TestPriorityQueue_3()
+    {
+        var priorityQueue = new PriorityQueue();
+         try
+        {
+            var value = priorityQueue.Dequeue();
+            Assert.Fail("Exception should have been thrown.");
+        }
+        catch (InvalidOperationException e)
+        {
+            Assert.AreEqual("The queue is empty.", e.Message);
+        }
+    }
 }
